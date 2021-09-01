ff939433507610162bc961127711d92cd631aef5
nguyentup <nguyentup@gmail.com>
2021-09-01 06:08:40 -0700

    Created constants folder

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import pygame
 from pygame.locals import *
+from typetest.constants import *
 import sys
 import time
 import random
 
 
 # 750 x 500    
-frame_per_second = 60 
     
 class main:
    
     def __init__(self):
-        self.w=750
-        self.h=500
+        self.width = width
+        self.height = height
         self.reset=True
         self.active = False
         self.input_text=''
@@ -23,26 +23,26 @@ class main:
         self.results = 'Time:0 Accuracy:0 % Wpm:0 '
         self.wpm = 0
         self.end = False
-        self.HEAD_C = (255,213,102)
-        self.TEXT_C = (240,240,240)
-        self.RESULT_C = (255,70,70)
+        self.HEAD_C = dandelion
+        self.TEXT_C = light_grey
+        self.RESULT_C = red
         
        
         pygame.init()
         self.open_img = pygame.image.load('openImg.png')
-        self.open_img = pygame.transform.scale(self.open_img, (self.w,self.h))
+        self.open_img = pygame.transform.scale(self.open_img, (self.width,self.height))
 
         self.bg = pygame.image.load('charcoal.jpeg')
         self.bg = pygame.transform.scale(self.bg, (750, 500))
 
-        self.screen = pygame.display.set_mode((self.w,self.h))
+        self.window = pygame.display.set_mode((self.width,self.height))
         pygame.display.set_caption('Testing Words Per Minute')
        
         
     def draw_text(self, screen, msg, y ,fsize, color):
         font = pygame.font.Font(None, fsize)
         text = font.render(msg, 1,color)
-        text_rect = text.get_rect(center=(self.w/2, y))
+        text_rect = text.get_rect(center=(self.width/2, y))
         screen.blit(text, text_rect)
         pygame.display.update()   
         
@@ -77,8 +77,8 @@ class main:
             # draw icon image
             self.time_img = pygame.image.load('reset.png')
             self.time_img = pygame.transform.scale(self.time_img, (150,150))
-            screen.blit(self.time_img, (self.w/2-75,self.h-140))
-            self.draw_text(screen,"Reset", self.h - 20, 26, (100,100,100))
+            screen.blit(self.time_img, (self.width/2-75,self.height-140))
+            self.draw_text(screen,"Reset", self.height - 20, 26, (100,100,100))
             
             print(self.results)
             pygame.display.update()
@@ -90,10 +90,10 @@ class main:
         self.running=True
         while(self.running):
             clock = pygame.time.Clock()
-            self.screen.fill((0,0,0), (50,250,650,50))
-            pygame.draw.rect(self.screen,self.HEAD_C, (50,250,650,50), 2)
+            self.window.fill((0,0,0), (50,250,650,50))
+            pygame.draw.rect(self.window,self.HEAD_C, (50,250,650,50), 2)
             # update the text of user input
-            self.draw_text(self.screen, self.input_text, 274, 26,(250,250,250))
+            self.draw_text(self.window, self.input_text, 274, 26,(250,250,250))
             pygame.display.update()
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -116,9 +116,9 @@ class main:
                     if self.active and not self.end:
                         if event.key == pygame.K_RETURN:
                             print(self.input_text)
-                            self.show_results(self.screen)
+                            self.show_results(self.window)
                             print(self.results)
-                            self.draw_text(self.screen, self.results,350, 28, self.RESULT_C)  
+                            self.draw_text(self.window, self.results,350, 28, self.RESULT_C)  
                             self.end = True
                             
                         elif event.key == pygame.K_BACKSPACE:
@@ -135,7 +135,7 @@ class main:
         clock.tick(frame_per_second)
 
     def reset_game(self):
-        self.screen.blit(self.open_img, (0,0))
+        self.window.blit(self.open_img, (0,0))
 
         pygame.display.update()
         time.sleep(1)
@@ -153,15 +153,15 @@ class main:
         self.word = self.get_sentence()
         if (not self.word): self.reset_game()
         #drawing heading
-        self.screen.fill((0,0,0))
-        self.screen.blit(self.bg,(0,0))
+        self.window.fill((0,0,0))
+        self.window.blit(self.bg,(0,0))
         msg = "Typing Speed Test"
-        self.draw_text(self.screen, msg,80, 80,self.HEAD_C)  
+        self.draw_text(self.window, msg,80, 80,self.HEAD_C)  
         # draw the rectangle for input box
-        pygame.draw.rect(self.screen,(255,192,25), (50,250,650,50), 2)
+        pygame.draw.rect(self.window,(255,192,25), (50,250,650,50), 2)
 
         # draw the sentence string
-        self.draw_text(self.screen, self.word,200, 28,self.TEXT_C)
+        self.draw_text(self.window, self.word,200, 28,self.TEXT_C)
         
         pygame.display.update()
 
---
+++ typetest/constants.py
@@ -0,0 +1,11 @@
+import pygame
+
+dandelion = (255,213,102) # title
+light_grey = (240,240,240) # sentences
+red = (255,70,70) # results
+
+# screen display 
+width = 750
+height = 500
+
+frame_per_second = 60 
