467af15facbcd71d5983be3f5cb94f8484622928
nguyentup <nguyentup@gmail.com>
2021-09-02 22:02:55 -0700

    Added message to user to avoid errors

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ class main:
         self._wpm = 0
         self._end = False
         self._header = aquamarine
+        self._details = aquamarine
         self._text = white
         self._result = bisque
         
-       
         pygame.init()
         self._open_img = pygame.image.load('openImg.png')
         self._open_img = pygame.transform.scale(self._open_img, (self._width, self._height))
@@ -35,52 +35,7 @@ class main:
 
         self._window = pygame.display.set_mode((self._width, self._height))
         pygame.display.set_caption('Testing Words Per Minute')
-       
-        
-    def _draw_text(self, screen, msg, y ,fsize, color):
-        font = pygame.font.Font(None, fsize)
-        text = font.render(msg, 1,color)
-        text_rect = text.get_rect(center=(self._width/2, y))
-        screen.blit(text, text_rect)
-        pygame.display.update()   
-        
-    def get_sentence(self):
-        f = open('sentences.txt').read()
-        sentences = f.split('\n')
-        sentence = random.choice(sentences)
-        return sentence
-
-    def show_results(self, screen):
-        if(not self._end):
-            # Time 
-            self._total_time = time.time() - self._time_start
-               
-            # Accuracy
-            count = 0
-            for i,c in enumerate(self._word):
-                try:
-                    if self._input_text[i] == c:
-                        count += 1
-                except:
-                    pass
-            self._accuracy = count/len(self._word)*100
-           
-            # WPM
-            self._wpm = len(self._input_text) * 60 / (5 * self._total_time)
-            self._end = True
-            print(self._total_time)
-                
-            self._results = "Time: " + str(round(self._total_time)) + " seconds   Accuracy: " + str(round(self._accuracy)) + "%" + "   WPM: " + str(round(self._wpm))
-
-            # reset
-            self._reset_image = pygame.image.load('reset.png')
-            self._reset_image = pygame.transform.scale(self._reset_image, (75, 75))
-            screen.blit(self._reset_image, (self._width / 2 - 35, self._height - 100))
-            self._draw_text(screen, "Reset", self._height - 35, 25, black)
-            
-            print(self._results)
-            pygame.display.update()
-
+    
     def run(self):
         self._reset_game()
     
@@ -90,7 +45,7 @@ class main:
             self._window.fill(black, text_input_rect)
             pygame.draw.rect(self._window,self._header, text_input_rect, 2)
             # update user input 
-            self._draw_text(self._window, self._input_text, 274, 26, white)
+            self.draw_text(self._window, self._input_text, 274, 26, white)
             pygame.display.update()
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -115,7 +70,7 @@ class main:
                             print(self._input_text)
                             self.show_results(self._window)
                             print(self._results)
-                            self._draw_text(self._window, self._results, 350, 28, self._result)  
+                            self.draw_text(self._window, self._results, 350, 28, self._result)  
                             self._end = True
                             
                         elif event.key == pygame.K_BACKSPACE:
@@ -128,8 +83,51 @@ class main:
             
             pygame.display.update()
              
-                
         clock.tick(frame_per_second)
+        
+    def draw_text(self, screen, message, y_coord ,size, color):
+        font = pygame.font.Font(None, size)
+        text = font.render(message, 1, color)
+        text_box = text.get_rect(center = (self._width / 2, y_coord))
+        screen.blit(text, text_box)
+        pygame.display.update()   
+        
+    def _get_sentence(self):
+        f = open('sentences.txt').read()
+        sentences = f.split('\n')
+        sentence = random.choice(sentences)
+        return sentence
+
+    def show_results(self, screen):
+        if(not self._end):
+            # Time 
+            self._total_time = time.time() - self._time_start
+               
+            # Accuracy
+            count = 0
+            for index, correct in enumerate(self._word):
+                try:
+                    if self._input_text[index] == correct:
+                        count += 1
+                except:
+                    pass
+            self._accuracy = count / len(self._word) * 100
+           
+            # WPM
+            self._wpm = len(self._input_text) * 60 / (5 * self._total_time)
+            self._end = True
+            print(self._total_time)
+                
+            self._results = "Time: " + str(round(self._total_time)) + " seconds   Accuracy: " + str(round(self._accuracy)) + "%" + "   WPM: " + str(round(self._wpm))
+
+            # reset
+            self._reset_image = pygame.image.load('reset.png')
+            self._reset_image = pygame.transform.scale(self._reset_image, (75, 75))
+            screen.blit(self._reset_image, (self._width / 2 - 35, self._height - 100))
+            self.draw_text(screen, "Reset", self._height - 35, 25, black)
+            
+            print(self._results)
+            pygame.display.update()
 
     def _reset_game(self):
         self._window.blit(self._open_img, (0,0))
@@ -147,18 +145,19 @@ class main:
         self._wpm = 0
 
         # Get random sentence 
-        self._word = self.get_sentence()
+        self._word = self._get_sentence()
         if (not self._word): self._reset_game()
         # heading 
         self._window.fill(black)
         self._window.blit(self._background, (0,0))
         message = "Type Fast and Accurately!!!"
-        self._draw_text(self._window, message, 80, 80, self._header)  
+        detailed_message = "**Make sure to click on the input box before you start**"
+        self.draw_text(self._window, message, 80, 80, self._header)  
+        self.draw_text(self._window, detailed_message, 120, 30, self._details)  
         # input box 
         pygame.draw.rect(self._window, aquamarine, text_input_rect, 2)
-
         # sentence
-        self._draw_text(self._window, self._word, 200, 28, self._text)
+        self.draw_text(self._window, self._word, 200, 28, self._text)
         
         pygame.display.update()
 
